db1fbc46c7662441ae4bc1aba8fa1c67e7a77ec4
Yasmin <yasminor@gmail.com>
2017-01-21 15:15:25 -0800

    WIP: creating nodes with model data

--- app/components/arbor-system.js
+++ app/components/arbor-system.js
@@ -1,19 +1,58 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // //
-  // //  main.js
-  // //
-  // //  A project template for using arbor.js
-  // Renderer: function(canvas) {
-  //   console.log(canvas);
-  // },
+    store: Ember.inject.service(),
 
-    Renderer: function(canvas, arbor){
+    // getSamples: function() {
+    //   return this.get('current_song').get('samples');
+    // }.property('current_song.samples')
+    // getSong: function() {
+    //   console.log(">>>>>>>>>>>>>>getting a song");
+    //   return this.get('song').get('song_title');
+    // }.property('song.song_title'),
+      // return this.get('store').findRecord('song', 70324).then(function(result) {
+      //   this.set('song_title', result.song_title);
+      // })
+
+    createNodeData: function() {
+      //get the current song's id
+      //use that id to findRecord in the store
+      console.log(">>>>>>>>>>>>>>getting a song!");
+      var current_song = this.get('current_song'); // internal model whatever that means
+      var current_id = current_song.get('id');
+      var current_song_title = current_song.get('song_title');
+
+
+      var current_samples = current_song.get('samples');
+      console.log("current id is: " + current_id);
+      console.log("current song is: " + current_song_title);
+      console.log("current samples are: " + current_samples);
+
+      for (var i=0; i<current_samples.length; i++) {
+        console.log(current_samples[i]);
+      }
+
+      // var data = {
+      //   "nodes": {
+      //     current_song_title: {},
+      //     current_song_sample_title: {},
+      //     current_song_sample_title: {}
+      //   },
+      //   "edges": {
+      //     current_song_title: { current_song_sample_title: {}, current_song_sample_title: {} }
+      //   }
+      // };
+      // return data;
+      // return this.get('store').findRecord('song', current_id); // don't really need this (findRecord), but may need it to find a sample's samples later
+    },
+
+    Renderer: function(canvas){
       canvas = $(canvas).get(0);
       var ctx = canvas.getContext("2d");
       var gfx = arbor.Graphics(canvas)
       var particleSystem;
+      // var kanye_song = this.get('store').get('song');
+      // console.log(">>>>>>>>>>>>>" + kanye_song);
 
       var that = {
         init: function(system){
@@ -62,24 +101,57 @@ export default Ember.Component.extend({
             ctx.stroke(); //draw it
           });
 
-          // var nodeBoxes = {};
+          var nodeBoxes = {};
           particleSystem.eachNode(function(node, pt){
             // node: {mass:#, p:{x,y}, name:"", data:{}}
             // pt:   {x:#, y:#}  node position in screen coords
 
+            // determine the box size and round off the coords if we'll be
+            // drawing a text label (awful alignment jitter otherwise...)
+            var label = node.data.shape||""
+            var w = ctx.measureText(""+label).width + 10
+            if (!(""+label).match(/^[ \t]*$/)){
+              pt.x = Math.floor(pt.x)
+              pt.y = Math.floor(pt.y)
+            }else{
+              label = null
+            }
             // draw a rectangle centered at pt
-            var w = 10;
+            // if (node.data.color) ctx.fillStyle = node.data.color
+            // else ctx.fillStyle = "rgba(0,0,0,.2)"
+            // if (node.data.color=='none') ctx.fillStyle = "white"
+
+            var w = 100;
             ctx.fillStyle = (node.data.sample_type) ? "orange" : "black";
             ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w);
 
+            // if (node.data.label=='dot'){
+            // gfx.oval(pt.x-w/2, pt.y-w/2, w,w, {fill:ctx.fillStyle})
+            // ctx.fillStyle = "white"
+            // ctx.fillText(node.data.song_title||"", pt.x, pt.y+4)
+            // nodeBoxes[node.name] = [pt.x-w/2, pt.y-w/2, w,w]
+            // }else{
+            // gfx.rect(pt.x-w/2, pt.y-10, w,20, 4, {fill:ctx.fillStyle})
+            // nodeBoxes[node.name] = [pt.x-w/2, pt.y-11, w, 22]
+            // }
+
             if (node.data.shape=='dot'){
             gfx.oval(pt.x-w/2, pt.y-w/2, w,w, {fill:ctx.fillStyle})
-            ctx.fillStyle = "white"
-            ctx.fillText(node.data.song_title||"", pt.x, pt.y+4)
-            // nodeBoxes[node.name] = [pt.x-w/2, pt.y-w/2, w,w]
+            nodeBoxes[node.name] = [pt.x-w/2, pt.y-w/2, w,w]
             }else{
             gfx.rect(pt.x-w/2, pt.y-10, w,20, 4, {fill:ctx.fillStyle})
-            // nodeBoxes[node.name] = [pt.x-w/2, pt.y-11, w, 22]
+            nodeBoxes[node.name] = [pt.x-w/2, pt.y-11, w, 22]
+            }
+
+            // draw the text
+            if (node.data.song_title){
+            ctx.font = "16px Helvetica"
+            ctx.textAlign = "center"
+            ctx.fillStyle = "white"
+            if (node.data.color=='none') ctx.fillStyle = '#333333'
+            ctx.fillText(node.data.song_title||"", pt.x, pt.y-8)
+            // ctx.fillText(kanye_song||"", pt.x, pt.y-8)
+            ctx.fillText(node.data.primary_artist||"", pt.x, pt.y+8)
             }
           });
         },
@@ -161,17 +233,23 @@ export default Ember.Component.extend({
     },
 
   didInsertElement() {
+    // var kanye_song = this.getSong();
+    // console.log(song);
+    // console.log(song.song_title);
+    // console.log(kanye_song.get('primary_artist'));
+
+
+
     var sys = arbor.ParticleSystem(1000, 600, 0.5); // create the system with sensible repulsion/stiffness/friction
-    // arbor.Graphics = Graphics;
     sys.parameters({gravity:true}); // use center-gravity to make the graph settle nicely
-    sys.renderer = this.Renderer("#viewport", arbor); // our newly created renderer will have its .init() method called shortly by sys...
+    sys.renderer = this.Renderer("#viewport"); // our newly created renderer will have its .init() method called shortly by sys...
 
     // add some nodes to the graph and watch it go...
     var data = {
       "nodes": {
-        "song": {mass:.5, fixed:true, x:500, y:100, song_title: "Thinkin Bout You", primary_artist: "Frank Ocean", samples: 5, shape: "dot"},
-        "sample1": {mass:1.5, song_title: "What You Want", primary_artist: "Logic", sample_type: false},
-        "sample2": {mass:1.5, song_title: "Drive By", primary_artist: "Eric Bellinger", sample_type: false},
+        "song": {mass:.5, fixed:true, x:500, y:100, song_title: "Thinkin Bout You", primary_artist: "Frank Ocean", samples: 5, 'shape': "dot"},
+        "sample1": {mass:1.5, song_title: "What You Want", primary_artist: "Logic", sample_type: false, 'shape': "dot"},
+        "sample2": {mass:1.5, song_title: "Drive By", primary_artist: "Eric Bellinger", sample_type: false, 'shape': "dot"},
         "sample3": {mass:1, song_title: "Thinking About Forever", primary_artist: "Bridget Kelly", sample_type: true}
 
       },
@@ -179,28 +257,13 @@ export default Ember.Component.extend({
         "song": {"sample1":{}, "sample2":{}, "sample3":{}}
       }
     };
+
+    //call a function that returns a data hash object of nodes and edges
+    //then call sys.graft(whatever the previous function returns)
+    var cs = this.createNodeData();
+    console.log(cs);
+
     sys.graft(data);
-    // sys.addNode('song', {mass:.5, fixed:true, x:500, y:100, song_title: "Thinkin Bout You", primary_artist: "Frank Ocean", samples: 5, shape: "circle"});
-    // sys.addNode('sample1', {mass:1.5, song_title: "What You Want", primary_artist: "Logic", sample_type: false});
-    // sys.addNode('sample2', {mass:1.5, song_title: "Drive By", primary_artist: "Eric Bellinger", sample_type: false});
-    // sys.addNode('sample3', {mass:1, song_title: "Thinking About Forever", primary_artist: "Bridget Kelly", sample_type: true});
-    // sys.addNode('sample4', {mass:1, song_title: "Thinking About You", primary_artist: "Tori Kelly"});
-    // sys.addNode('sample5', {mass:1, song_title: "Thinkin bout you", primary_artist: "Daniela Andrade"});
-    //
-    // sys.addEdge('song', 'sample1', {length: 35});
-    // sys.addEdge('song', 'sample2', {length: 35});
-    // sys.addEdge('song', 'sample3', {length: 80});
-    // sys.addEdge('song', 'sample4', {length: 80});
-    // sys.addEdge('song', 'sample5', {length: 80});
-
-
-
-
-    // sys.addEdge('a','b');
-    // sys.addEdge('a','c');
-    // sys.addEdge('a','d');
-    // sys.addEdge('a','e');
-    // sys.addNode('f', {alone:true, mass:.25});
 
   }
 });
